f32839590ddafb056d00104428e761570d2e64e8
dkisliuk <d.kisliuk@yahoo.ca>
2026-01-20 14:37:06 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Plotting
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
